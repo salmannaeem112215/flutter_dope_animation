d21e526fd16a6022dcfd8da2632aef48fe14e644
Salman <salman.naeem.112215@gmail.com>
2022-09-01 07:48:18 +0500

    Added Lottie dependicies and animation

--- lib/myHomePage.dart
+++ lib/myHomePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
@@ -11,7 +12,10 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('hi'),
+      body: Center(
+        child: Lottie.network(
+            "https://assets4.lottiefiles.com/datafiles/SkdS7QDyJTKTdwA/data.json"),
+      ),
     );
   }
 }
